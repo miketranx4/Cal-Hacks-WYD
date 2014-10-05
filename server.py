8d8d9f95bf129e86da2d6ce5c49cd985b7c6e037
Ubuntu <azureuser@wyd-calhacks.wyd-calhacks.b4.internal.cloudapp.net>
2014-10-05 09:28:14 +0000

    Serve index.html from root

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+
+# For environment variables
 from os import getenv
+
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     app.run(port=(int(getenv('SERVER_PORT')) or 5000), host='0.0.0.0')
